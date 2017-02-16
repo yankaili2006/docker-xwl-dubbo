7586bf145f1ff0ae0fe0ae1220ce8dd241d2fb66
yankaili2006 <yankaili2006@163.com>
2017-02-16 23:30:24 +0800

    xwl-dubbo

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ APP=xwl-dubbo
 VERSION=1.0-SNAPSHOT
 JAR=$APP-$VERSION-jar-with-dependencies.jar
 
-JAR_PATH=$(curl "http://101.200.176.31:8181/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION&c=jar-with-dependencies" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
+JAR_PATH=$(curl "http://101.200.176.31:8082/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION&c=jar-with-dependencies" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
 
-NEXUS="http://101.200.176.31:8181/nexus/content/repositories/snapshots/$JAR_PATH"
+NEXUS="http://nexus.zhixuntongda.com/nexus/content/repositories/snapshots/$JAR_PATH"
 
 wget $NEXUS -O $JAR
 
