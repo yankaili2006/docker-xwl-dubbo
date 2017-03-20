5adec3700a4945d57a3d4fd3ad1ce445eed02c91
yankaili2006 <yankaili2006@163.com>
2017-03-20 09:37:37 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ JAR=$APP-$VERSION-jar-with-dependencies.jar
 
 JAR_PATH=$(curl "http://101.200.176.31:8082/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION&c=jar-with-dependencies" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
 
-NEXUS="http://nexus.zhixuntongda.com/nexus/content/repositories/snapshots/$JAR_PATH"
+NEXUS="http://101.200.176.31:8082/nexus/content/repositories/snapshots/$JAR_PATH"
 
 wget $NEXUS -O $JAR
 
